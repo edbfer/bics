3f06b04062ad00f12928a91326af980684e9ff08
afonso caçador <afonso.cacador@tecnico.ulisboa.pt>
2018-01-20 19:59:36 +0000

    field.h passou pa templates (finale)

--- field.h
+++ field.h
@@ -15,15 +15,90 @@ class field
 
   int getOffset(int* coords);
 
+  
+
  public:
   field(int D, int col);
 
   ~field();
-
 	        
   T getValue(int* coords);
   void setValue(T v, int* coords);
   void fill(ifstream& in);
   void print(ofstream& in);
-  //void setCharge()
+
+  T getValue(int* coords)
+  {
+    return campo[getOffset(coords)];	
+  }
+
+  void setValue(T v, int* coords)
+  {
+    campo[getOffset(coords)] = v;
+  }
+
+  int getOffset(int* coords)
+  {
+    int offset = coords[0];
+    for(int i = 1; i<D; i++)
+      {
+	offset += (coords[i] * pow(this->col, i));
+      }
+
+    return offset;
+  }
+
+  
+  T fill(ifstream& in)
+  {
+    int *coords = new int[D]();
+
+    while(!in.eof())
+      {
+	for(int i = 0; i<D; i++)
+	  {
+	    in >> coords[i];
+	  }
+
+	int offset = getOffset(coords);
+
+	in >> campo[offset];
+      }
+
+    delete[] coords;
+  }
+
+  T print(ofstream& out)
+  {
+    int *coords = new int[this->D]();
+    int max = (int) pow((double)this->col, (double)this->D);
+
+    for(int i = 0; i<max; i++)
+      {
+
+	int j = 0;
+	for(j = 0; j<D; j++)
+	  {
+	    out << coords[j] << "\t";
+	  }
+
+	int of = getOffset(coords);
+	out << campo[of] << endl;
+
+	j = 0;
+	while(j<D)
+	  {
+	    if(coords[j] < (this->col-1))
+	      {
+		coords[j]++;
+		break;
+	      }
+	    else
+	      {
+		coords[j] = 0;
+	      }
+	    j++;
+	  }
+      }
+  }  
 };
