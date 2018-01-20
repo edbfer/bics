4facce2a7b569027ff70ddb0e6674ba2bc34bd30
afonso caçador <afonso.cacador@tecnico.ulisboa.pt>
2018-01-20 14:07:23 +0000

    class de complexos usa templates. funções definidas dentro da classe

--- complex.h
+++ complex.h
@@ -13,29 +13,99 @@ class complex
 	T im;
 
 public:
-	__host__ __device__ complex();
-	__host__ __device__ complex(int real);
-	__host__ __device__ complex(double real);
-	__host__ __device__ complex(double real, double im);
+	__host__ __device__ complex() : real(0.), im(0.) {}
+	__host__ __device__ complex(int real) : real(real), im(0.) {}
+	__host__ __device__ complex(T real) : real(real), im(0.) {}
+	__host__ __device__ complex(T real, T im) : real(real), im(0.) {}
 	
-	__host__ __device__ friend complex operator+(complex& c1, complex& c2);
-	__host__ __device__ friend complex operator-(complex& c1, complex& c2);
-	__host__ __device__ complex operator-();
-	__host__ __device__ complex operator~();
-	__host__ __device__ friend complex operator*(complex& c1, complex& c2);
-	__host__ __device__ friend complex operator*(double a, complex& c1);
+	
+	__host__ __device__ friend complex operator+(complex& c1, complex& c2)
+	{
+	  complex res(0, 0);
+	  res.real = c1.real + c2.real;
+	  res.im = c1.im + c2.im;
+	  return res;
+	}
+	
+	__host__ __device__ friend complex operator-(complex& c1, complex& c2)
+	{
+	  complex res(0, 0);
+	  res.real = c1.real - c2.real;
+	  res.im = c1.im - c2.im;
+	  return res;
+	}
+
+	__host__ __device__ complex operator-()
+	{
+	  this->real *= -1;
+	  this->im *= -1;
+	  return *this;
+	}
+	
+	__host__ __device__ complex operator~()
+	{
+	  im = -im;
+	  return *this;
+	}
+	
+	__host__ __device__ friend complex operator*(complex& c1, complex& c2)
+	  {
+	    complex res(0, 0);
+	    res.real = c1.real * c2.real - c1.im * c2.im;
+	    res.im = c1.im * c2.real + c1.real * c2.im;
+	    return res;
+	  }
+	
+	__host__ __device__ friend complex operator*(T a, complex& c1);
+	{
+	  complex res(0, 0);
+	  res.real = a * c1.real;
+	  res.im = a * c1.im;
+	  return res;
+	}
+	
 	__host__ __device__ friend complex operator/(complex& c1, complex& c2);
-	__host__ __device__ friend complex operator/(complex& c1, double a);
+	{
+	  complex res(0, 0);
+	  T m = c2.real * c2.real + c2.im * c2.im;
+	  res.real = (c1.real * c2.real + c1.im * c2.im)/m;
+	  res.im	= (c1.im * c2.real - c1.real * c2.im)/m;
+	  return res;
+	}
+	
+	__host__ __device__ friend complex operator/(complex& c1, T a);
+	{
+	  complex res(0, 0);
+	  res.real = c1.real/a;
+	  res.im = c1.im/a;
+	  return res;
+	}
 
 	__host__ __device__ friend int operator==(complex& c1, complex& c2);
-
-	__host__ __device__ operator double()
 	{
-		return real;
+	  if ((c1.real == c2.real) && (c1.im == c2.im))
+	    return 1;
+	  return 0;
+	}
+
+	__host__ __device__ operator T()
+	{
+	  return real;
 	}
 
 	__host__ __device__ friend ostream& operator<<(ostream& out, complex& c1);
+	{
+	  const char* sign = (c1.im > 0) ? "+" : ((c1.im < 0) ? "-" : "+");
+	  out << c1.real << sign << fabs(c1.im) << "i";
+	  return out;
+	}
+	
 	__host__ __device__ friend istream& operator>>(istream& in, complex& c1);
+	{
+	  in >> c1.real >> c1.im;
+	  return in;
+	}
 
-	double mod();
+
+	T mod();
 };
