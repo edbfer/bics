7982a32193adb4d99650e2deb99b615ea71cf19f
afonso caçador <afonso.cacador@tecnico.ulisboa.pt>
2018-01-24 20:03:55 +0000

    e5 lastest "improvement"

--- e5.cpp
+++ e5.cpp
@@ -13,6 +13,8 @@ int main(int argc, char const *argv[])
 
 	matriz<float> lattice(128, 128);
 
+	complex<float> i(0, 1);
+
 	ifstream f("params.txt");
 	f >> gamma >> omega >> G;
 	f.close();
@@ -37,16 +39,31 @@ int main(int argc, char const *argv[])
 
 	ofstream init("initial.txt");
 	lattice.printCoord(init);
-	init.close();	
+	init.close();
 
 	math::h = 20.0/128.0;
 
 	//complex<float> norm = simpson2d<float>(lattice);
-	lattice = dx<float>(lattice);
+	 derivuxa= dx<float>(lattice);
+	 derivuya= dy<float>(lattice);
 	//lattice = dy<float>(lattice);
 
 	ofstream res("result.txt");
 	lattice.printCoord(res);
+
+	matriz<float> lapx= d2x<float> (lattice);
+	matriz<float> lapy= d2y <float>(lattice);
+	matriz<float> lap = lapx + lapy;
+
+	matriz<float> laplacianu(128,128)= (-0.5*lap);
+	matriz<float> paressela2(128,128)= ((x*x + y*y)/2)*lattice;
+	matriz<float> paressela3(128,128)= G*norm*norm*lattice;
+
+	matriz<float> parssialex(128,128)=y*dx;
+	matriz<float> parssialey(128,128)=x*dy;
+
+	matriz<float> ultimeparcele(128,128)= i*omega*(parssialey-parssialex);
+
 
 	return 0;
 }
